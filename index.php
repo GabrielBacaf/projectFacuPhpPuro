671c1a4cc0314ac9d04f4646d6f6edfc35173571
ederbellini <161244121+ederbellini@users.noreply.github.com>
2025-09-08 00:38:15 -0400

    Update index.php
    adicionando o botão de procura e alguns ajustes pro alinhamento ficar mais bacana com essa nova funcionalidade

--- index.php
+++ index.php
@@ -9,16 +9,23 @@ ob_start(); // começa a capturar o HTML
 
 <section>
     <div class="row">
-        <?php include_once __DIR__ . '/Pages/fragments/session.php'; ?> 
+        <?php include_once __DIR__ . 
+'/Pages/fragments/session.php'; ?> 
     </div>
-    <div class="button-azul">
-        <form class="button" action="controller/bookController.php" method="GET" style="display:inline">
+    <div class="main-header">
+        <form action="index.php" method="GET">
+            <input type="text" name="search" placeholder="Buscar por título, autor, gênero...">
+            <button type="submit">Buscar</button>
+        </form>
+        <form class="button" action="controller/bookController.php" method="GET">
             <input type="hidden" name="action" value="create">
             <button type="submit">Cadastrar </button>
         </form>
     </div>
+
     <div>
-        <?php include_once __DIR__ . '/Pages/livro/listaLivros.php'; ?>
+        <?php include_once __DIR__ . 
+'/Pages/livro/listaLivros.php'; ?>
     </div>
     
     
@@ -26,5 +33,6 @@ ob_start(); // começa a capturar o HTML
 
 <?php
 $conteudo = ob_get_clean(); // guarda o HTML capturado
-include_once __DIR__ . '/Pages/fragments/layout.php'; // inclui o layout
+include_once __DIR__ . 
+'/Pages/fragments/layout.php'; // inclui o layout
 ?>
