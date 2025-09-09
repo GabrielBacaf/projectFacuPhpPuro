1dd0bee2b617c175077f3c467296658852723616
TuzinHPCB <arthurinreallife@gmail.com>
2025-09-08 20:38:15 -0400

    Correção na tela de deição no session.php + alteração no seed.php das imagens

--- Pages/fragments/session.php
+++ Pages/fragments/session.php
@@ -1,27 +1,21 @@
-  <?php
-    session_status() === PHP_SESSION_NONE ? session_start() : null;
+<?php
 
-    if (isset($_SESSION['success'])) {
-        echo "<div class='flash-message success'>" . $_SESSION['success'] . "</p>";
-        unset($_SESSION['success']);
-    }
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-    if (isset($_SESSION['error'])) {
-        echo "<div class='flash-message error'>" . $_SESSION['error'] . "</p>";
-        unset($_SESSION['error']);
-    }
-    ?>
-  <!-- 
- <?php
-    session_status() === PHP_SESSION_NONE ? session_start() : null;
 
-    if (isset($_SESSION['success'])) {
-        echo "<div class='flash-message success'>" . htmlspecialchars($_SESSION['success']) . "</div>";
-        unset($_SESSION['success']);
-    }
+if (isset($_SESSION['success'])) {
+    
+    echo "<div class='flash-message success'>" . htmlspecialchars($_SESSION['success']) . "</div>";
+    
+   
+    unset($_SESSION['success']);
+}
 
-    if (isset($_SESSION['error'])) {
-        echo "<div class='flash-message error'>" . htmlspecialchars($_SESSION['error']) . "</div>";
-        unset($_SESSION['error']);
-    }
-    ?> -->
+
+if (isset($_SESSION['error'])) {
+    echo "<div class='flash-message error'>" . htmlspecialchars($_SESSION['error']) . "</div>";
+    unset($_SESSION['error']);
+}
+?>
