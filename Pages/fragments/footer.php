1605156217a042423f7eb4312b7bb167b3b3251d
TuzinHPCB <arthurinreallife@gmail.com>
2025-09-08 21:01:32 -0400

    Mudança no footer

--- Pages/fragments/footer.php
+++ Pages/fragments/footer.php
@@ -1,3 +1,3 @@
 <div class="footer-container">
-    <p>Trabalho desenvolvido como avaliação prática da disciplina WEB1, desenvolvido com carinho pelo grupo CRIMPADORES!!.</p>
+    <p>Trabalho desenvolvido como avaliação prática da disciplina WEB1, codado com carinho pelo grupo CRIMPADORES!!.</p>
 </div>
